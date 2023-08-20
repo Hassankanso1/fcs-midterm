cc1f557772bc46b3959cea541b1e14899b35b9f2
Hassankanso1 <hassankanso1@outlook.com>
2023-08-20 20:44:29 +0300

    replace function 2 to display emp mneu

--- midterm-solution.py
+++ midterm-solution.py
@@ -3,24 +3,40 @@ def uploading():
     data={}
     with open ("database.txt", "r") as f:    
         for line in f: 
-            [employee_id, username, timestamp, gender, salary]=line.strip().split(',') #https://www.freecodecamp.org/news/the-string-strip-method-in-python-explained/
-            data[employee_id]={'username':username,
+            [employee_id, username, timestamp, gender, salary]=line.split(', ') #https://www.freecodecamp.org/news/the-string-strip-method-in-python-explained/
+            data[username]={'employee id':employee_id,
                                'timestamp':timestamp,
                                'gender':gender,
                                'salary':int(salary)}
         return data
-#print(uploading())
+    
+#function 2 : employee Menu defined
+def employee_menu(data,username):
+    if data[username]["gender"]=="male":
+        print("Hi Mr. " + username)
+    else:
+        print("Hi Ms. " + username)
+    print("1. Check my salary")
+    print("2. Exit")
+    choose=int(input("Choose: "))
+    print(choose)
+    if choose==1:
+        print(data[username]["salary"])
 
+    elif choose==2:
+        menu()   
 
-#function 2: menu display
+#last funciton: menu display
 def menu():
+    data=uploading()
+    print("cdaoud" in data.values())
     print("Welcome User")
     user=input("enter username: ")
     password=input("enter password: ")
     if user=="admin" and password=="admin123123":
         admin_menu()
-    elif user in (database) username:
-        employee_menu
+    elif user in data.keys():
+        employee_menu(data,user)
 
-        
-print(menu())
+
+menu()
