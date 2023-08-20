11895fa7c7c9038124670fcb7f28aff5d10a8059
Hassankanso1 <hassankanso1@outlook.com>
2023-08-20 19:05:01 +0300

    fucntion 2 added

--- midterm-solution.py
+++ midterm-solution.py
@@ -9,4 +9,18 @@ def uploading():
                                'gender':gender,
                                'salary':int(salary)}
         return data
-print(uploading())
+#print(uploading())
+
+
+#function 2: menu display
+def menu():
+    print("Welcome User")
+    user=input("enter username: ")
+    password=input("enter password: ")
+    if username=="admin" or password=="admin123123":
+        admin_menu()
+    elif user== username:
+        employee_menu
+
+        
+print(menu())
