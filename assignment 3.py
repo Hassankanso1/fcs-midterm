721fe13d532c401bc1449d5351b6f74fdbf90a5c
Hassankanso1 <hassankanso1@outlook.com>
2023-09-09 18:28:47 +0300

    cash wash exercise added

--- assignment 3.py
+++ assignment 3.py
@@ -40,3 +40,65 @@ print(balance(input2))
 print(balance(input3)) 
 print(balance(input4))
 print(balance(input5))
+
+#Q2: Cash Wash Exercise
+class Car:
+    def __init__(self, make, color, plate_number):
+        self.make = make
+        self.color = color
+        self.plate_number = plate_number
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, car):
+        self.queue.append(car)
+    def dequeue(self):
+        if not self.isEmpty():
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+    def isEmpty(self):
+        return len(self.queue) == 0
+    def front(self):
+        if not self.isEmpty():
+            return self.queue[0]
+        else:
+            return None
+
+car_wash_queue = Queue()
+
+while True:
+    print("Car Wash Queue Program")
+    print("1. Insert a car to the queue")
+    print("2. Remove the car from the queue")
+    print("3. Quit")
+
+    choice = input("Choose your option: ")
+
+    if choice == "1":
+        make = input("Enter car make: ")
+        color = input("Enter car color: ")
+        plate_number = int(input("Enter car plate number: "))
+        car = Car(make, color, plate_number)
+        car_wash_queue.enqueue(car)
+        print(make + " " + color + " (Plate: " + str(plate_number) + ") added to the queue.")
+
+    elif choice == "2":
+        if not car_wash_queue.isEmpty():
+            removed_car = car_wash_queue.dequeue()
+            print("Car removed from the queue:")
+            print("Make: " + removed_car.make)
+            print("Color: " + removed_car.color)
+            print("Plate Number: " + str(removed_car.plate_number))
+        else:
+            print("No cars in the queue.")
+
+    elif choice == "3":
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid option")
--- test.py
+++ test.py
@@ -1,23 +1,61 @@
-#file = open("database.txt", "r")
-#x=f.readline(1)
-#print(x)
+class Car:
+    def __init__(self, make, color, plate_number):
+        self.make = make
+        self.color = color
+        self.plate_number = plate_number
 
-#with open("database.txt", "r") as f:
-#    print(f.readline())
-#    print(f.readline())
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, car):
+        self.queue.append(car)
+    def dequeue(self):
+        if not self.isEmpty():
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+    def isEmpty(self):
+        return len(self.queue) == 0
+    def front(self):
+        if not self.isEmpty():
+            return self.queue[0]
+        else:
+            return None
 
-#with open("database.txt", "r") as f:
-#    print(f.readlines(1))
+car_wash_queue = Queue()
 
+while True:
+    print("Car Wash Queue Program")
+    print("1. Insert a car to the queue")
+    print("2. Remove the car from the queue")
+    print("3. Quit")
 
+    choice = input("Choose your option: ")
 
-#funnction 1: read data
-def uploading(data):
-    data={}
-    with open ("database.txt", "r") as f:    #https://www.youtube.com/watch?v=2dUu7r14JwM
-        for line in f: 
-            employee_id, username, timestamp, gender, salary=line.strip().split(',')
-            data[employee_id]={'username'=username,'timestamp'=timestamp,'gender'=gender,'salary'=int(salary)}
-        return data
-    
+    if choice == "1":
+        make = input("Enter car make: ")
+        color = input("Enter car color: ")
+        plate_number = int(input("Enter car plate number: "))
+        car = Car(make, color, plate_number)
+        car_wash_queue.enqueue(car)
+        print(make + " " + color + " (Plate: " + str(plate_number) + ") added to the queue.")
+
+    elif choice == "2":
+        if not car_wash_queue.isEmpty():
+            removed_car = car_wash_queue.dequeue()
+            print("Car removed from the queue:")
+            print("Make: " + removed_car.make)
+            print("Color: " + removed_car.color)
+            print("Plate Number: " + str(removed_car.plate_number))
+        else:
+            print("No cars in the queue.")
+
+    elif choice == "3":
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid option")
 
